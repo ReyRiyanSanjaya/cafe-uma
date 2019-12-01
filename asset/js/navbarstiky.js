19d10da13eb008e1bd8009e5131a9e6c5e2499f5
Rey Riyan Sanjaya <reyriyansanjaya>
2019-12-01 05:07:23 -0800

    perubahaan bebeerapa file

--- asset/js/navbarstiky.js
+++ asset/js/navbarstiky.js
@@ -1,16 +1,3 @@
-//   navbar interaksi
-jQuery(document).ready(function() {
-
-    "use strick"
-
-    $('header').ripples({
-        dropRadius: 10,
-        pertubance: 0.01,
-    })
-
-});
-// navbar 
-
 $(window).on('scroll', function() {
     if ($(window).scrollTop()) {
         $('nav').addClass('sticky');
