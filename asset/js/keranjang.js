8fae2098efb2f45b13c2d3edd886264f1cf03244
Rey Riyan Sanjaya <reyriyansanjaya>
2019-12-03 01:39:33 -0800

    perbuhaan beberapa style

--- asset/js/keranjang.js
+++ asset/js/keranjang.js
@@ -1,6 +1,6 @@
 function openNav() {
-    document.getElementById("keranjang-nav").style.width = "300px";
-    document.getElementById("main").style.marginLeft = "300px";
+    document.getElementById("keranjang-nav").style.width = "550px";
+    document.getElementById("main").style.marginLeft = "550px";
     document.body.style.filter = "rgba(0,0,0,0.4)";
     document.getElementById("bodyhtml").style.filter = "grayscale(100%)";
 }
