52102dace1dd3c245e70c21c0bc4d3c81c589c1e
Rey Riyan Sanjaya <reyriyansanjaya>
2019-12-02 21:59:26 -0800

    penambahan side bar shopp si reza request

---
+++ asset/js/keranjang.js
@@ -0,0 +1,13 @@
+function openNav() {
+    document.getElementById("keranjang-nav").style.width = "300px";
+    document.getElementById("main").style.marginLeft = "300px";
+    document.body.style.filter = "rgba(0,0,0,0.4)";
+    document.getElementById("bodyhtml").style.filter = "grayscale(100%)";
+}
+
+function closeNav() {
+    document.getElementById("keranjang-nav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    document.body.style.backgroundColor = "white";
+}
+// buat keranjang belanjaan
